0d25b4fbb7d5baf6c3354cc7485c18a3ff9fba17
Mrs. Lombardi <lombardim@theellisschool.org>
2019-01-14 11:12:18 -0500

    U3L02 Notes

--- src/GuessMyColor.java
+++ src/GuessMyColor.java
@@ -3,15 +3,32 @@ import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.EventQueue;
 import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.UIManager;
 
 public class GuessMyColor extends JFrame{
 	
+	//computer's color - randomly generated
+	private int compRed = 0;
+	private int compGreen = 0;
+	private int compBlue = 0;
+	private Color compColor = new Color (compRed, compGreen, compBlue);
+	
+	
+	//my color
+	private int myRed = 0;
+	private int myGreen = 0;
+	private int myBlue = 0;
+	private Color myColor = new Color (myRed, myGreen, myBlue);
+	
+	private JPanel myPanel = new JPanel();
 
 	public GuessMyColor(){	
 		
@@ -51,13 +68,13 @@ public class GuessMyColor extends JFrame{
 		
 		Dimension size = new Dimension(50, 50);
 		
-		JPanel myPanel = new JPanel();
-		myPanel.setBackground(Color.BLACK); //this will change with buttons
+		myPanel.setBackground(myColor); //this will change with buttons
 		myPanel.setPreferredSize(size);
 		centerPanel.add(myPanel, BorderLayout.PAGE_START);
 		
 		JPanel computerPanel = new JPanel();
-		computerPanel.setBackground(Color.YELLOW); //will have to be randomized color
+		generateRandomColor();
+		computerPanel.setBackground(compColor); //will have to be randomized color
 		computerPanel.setPreferredSize(size);
 		centerPanel.add(computerPanel, BorderLayout.PAGE_END);
 		
@@ -69,26 +86,129 @@ public class GuessMyColor extends JFrame{
 		//buttons
 		JButton moreRedButton = new JButton("+");
 		moreRedButton.setBackground(Color.RED);
-		buttonPanel.add(moreRedButton);
+		//action listener
+		moreRedButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				addRed();
+			}
+		});
+		buttonPanel.add(moreRedButton); //add button to panel LAST
+		
 		JButton lessRedButton = new JButton("-");
 		lessRedButton.setBackground(Color.RED);
-		buttonPanel.add(lessRedButton);
+		//action listener
+		lessRedButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				subtractRed();
+				}
+		});		
+		buttonPanel.add(lessRedButton); 
 
 		
 		JButton moreGreenButton = new JButton("+");
 		moreGreenButton.setBackground(Color.GREEN);
+		moreGreenButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				addGreen();
+				}
+		});			
 		buttonPanel.add(moreGreenButton);
+		
 		JButton lessGreenButton = new JButton("-");
 		lessGreenButton.setBackground(Color.GREEN);
+		lessGreenButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				subtractGreen();
+				}
+		});	
 		buttonPanel.add(lessGreenButton);
 		
 		JButton moreBlueButton = new JButton("+");
 		moreBlueButton.setBackground(Color.BLUE);
+		moreBlueButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				addBlue();
+				}
+		});	
 		buttonPanel.add(moreBlueButton);
+		
 		JButton lessBlueButton = new JButton("-");
 		lessBlueButton.setBackground(Color.BLUE);
+		lessBlueButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				subtractBlue();
+				}
+		});	
 		buttonPanel.add(lessBlueButton);
-
+	}
+	
+	private void generateRandomColor() {
+		compRed = ((int)(Math.random() * 15 + 1) * 17);
+		compGreen = ((int)(Math.random() * 15 + 1) * 17);
+		compBlue = ((int)(Math.random() * 15 + 1) * 17);
+		
+		compColor = new Color(compRed, compGreen, compBlue);
+		System.out.println(compRed + " " + compGreen + " " + compBlue);
+	}
+	
+	private void addRed() {
+		myRed += 17;
+		if (myRed > 255) {
+			myRed = 0;
+		}
+		updateColor();
+	}
+	
+	private void subtractRed() {
+		myRed -= 17;
+		if (myRed < 0) {
+			myRed = 255;
+		}
+		updateColor();
+	}
+	
+	private void addGreen() {
+		myGreen += 17;
+		if (myGreen > 255) {
+			myGreen = 0;
+		}
+		updateColor();
+	}
+	
+	private void subtractGreen() {
+		myGreen -= 17;
+		if (myGreen < 0) {
+			myGreen = 255;
+		}
+		updateColor();
+	}
+	
+	private void addBlue() {
+		myBlue += 17;
+		if (myBlue > 255) {
+			myBlue = 0;
+		}
+		updateColor();
+	}
+	
+	private void subtractBlue() {
+		myBlue -= 17;
+		if (myBlue < 0) {
+			myBlue = 255;
+		}
+		updateColor();
+	}
+	
+	private void updateColor() {
+		myColor = new Color (myRed, myGreen, myBlue);
+		myPanel.setBackground(myColor);
+		
+		if (myRed == compRed &&
+				myGreen == compGreen &&
+				myBlue == compBlue) {
+			JOptionPane.showMessageDialog(null, "Congratulations! You win!");
+		}
+		
 	}
 
 	public static void main(String[] args) {
